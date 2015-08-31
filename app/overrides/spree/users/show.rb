8ac7b3c2cef2521ecbc0fdefde4a575e4618279a
Ian Manseau <manseauian@gmail.com>
2015-08-31 20:26:30 +0000

    Added Social-Likes, nice social buttons. Added to the my account to share referral link.

--- app/overrides/spree/users/show.rb
+++ app/overrides/spree/users/show.rb
@@ -4,29 +4,22 @@ Deface::Override.new(
   :insert_after => "[data-hook='account_summary'] #user-info dd:first"
   ) do
     <<-CODE.chomp
-    
-    <!-- Facebook Script for the share button -->
-    <div id="fb-root"></div>
-      <script>
-      (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-          js = d.createElement(s); js.id = id;
-          js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4&appId=702517669822721";
-          fjs.parentNode.insertBefore(js, fjs);
-          }(document, 'script', 'facebook-jssdk'));
-        </script>
-    
+    <dt>Spread The Depravity</dt>
+      <dd>Refer your friends and earn free shit!</dd>
+      
     <dt>Referral URL</dt>
       <dd><input type='text' value='<%= referral_url(@user.referral.code) %>' onClick='this.select();' /></dd>
-          
-        <!-- Facebook Share Button -->
-      <div class="fb-share-button" data-href="<%= referral_url(@user.referral.code) %>" data-layout="button"></div>
-      
-      <!-- Twitter Share Button -->
-      <a href="https://twitter.com/share" class="twitter-share-button" data-url="<%= referral_url(@user.referral.code) %>" data-text="A game pushing humanity. Highlight the deepest darkest corners of your friends minds!" data-count="none">Tweet</a>
-      <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>  
-        
+
+      <dd>
+      <!-- Start Social-Like Buttons -->
+      <div class="social-likes" data-url="<%= referral_url(@user.referral.code) %>" data-title="Party game pushing humanity. Highlighting the deepest darkest corners of minds!">
+        <div class="facebook" title="Share link on Facebook">Facebook</div>
+        <div class="twitter" data-via="BeDepraved" title="Share link on Twitter">Twitter</div>
+        <div class="plusone" title="Share link on Google+">Google+</div>
+      </div>
+      <!-- End Social-Like Buttons -->
+      </dd>
+
       <!-- Switched from user count to sales count -->
       <dt>Referred Sales</dt>
       <dd><%= @user.referral.referred_orders.count %></dd>
